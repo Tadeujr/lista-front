0ab7ebffc143bb736a9f42b8a113269eb2a01df4
Tadeujr <tadeu_junior@outlook.com>
2023-01-26 17:32:49 -0300

    primeiro teste com as variaveis do back

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,22 +1,36 @@
+import { json } from "node:stream/consumers";
 import Header from "../components/header";
 import styles from "../styles/Table.module.css";
 
 export default function Table() {
+  const list2 =     {
+    "store": "Carrefour",
+    "productName": "Arroz",
+    "price": 15.99,
+    "buyDate": "12/12/2012",
+    "unity": 1,
+    "wasAcquired": true,
+    "listId": 10
+}
+console.log(list2.store)
+ 
   const lista = {
     Feijao: {
       info: {
+        loja:"Carrefour",
         produto: "Feijão",
         marca: "juju",
         preco: "4.99",
         DataCompra: "27/05/2022",
         unidade: "1 kg",
         qtd: "20",
-        comprado: true,
+        comprado: false,
       },
     },
 
     Arroz: {
       info: {
+        loja:"Carrefour",
         produto: "Arroz",
         marca: "Sepé",
         preco: "17.90",
@@ -37,42 +51,46 @@ export default function Table() {
         <table  className={styles.tbZebra}>
           <thead>
             <tr>
+              <th>Loja</th>
               <th>Produto</th>
               <th>Marca</th>
               <th>Preço</th>
               <th>Data de Compra</th>
-              <th>Unidade</th>
+              <th>Medida</th>
               <th>Quantidade</th>
               <th>Comprado</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>{lista.Arroz.info.produto}</td>
-              <td>{lista.Arroz.info.marca}</td>
-              <td>{lista.Arroz.info.preco}</td>
-              <td>{lista.Arroz.info.unidade}</td>
-              <td>{lista.Arroz.info.qtd}</td>
-              <td>{lista.Arroz.info.DataCompra}</td>
-              <td><input className={styles.container} type="checkbox"  name="bought"/></td>
+            <td>{list2.store}</td>
+              <td>{list2.productName}</td>
+              <td>{'Sepe'}</td>
+              <td>{list2.price}</td>
+              <td>{list2.buyDate}</td>
+              <td>{list2.unity}</td>
+              <td>{"2"}</td>
+              <td><input className={styles.container} type="checkbox" checked={list2.wasAcquired} name="bought"/></td>
             </tr>
 
             <tr>
+            <td>{lista.Feijao.info.loja}</td>
               <td>{lista.Feijao.info.produto}</td>
               <td>{lista.Feijao.info.marca}</td>
               <td>{lista.Feijao.info.preco}</td>
+              <td>{lista.Feijao.info.DataCompra}</td>
               <td>{lista.Feijao.info.unidade}</td>
               <td>{lista.Feijao.info.qtd}</td>
-              <td>{lista.Feijao.info.DataCompra}</td>
-              <td><input className={styles.container} type="checkbox"  name="bought"/></td>
+              <td><input className={styles.container} type="checkbox"  checked={lista.Feijao.info.comprado} name="bought"/></td>
             </tr>
             <tr>
+              <td>{lista.Arroz.info.loja}</td>
               <td>{lista.Arroz.info.produto}</td>
               <td>{lista.Arroz.info.marca}</td>
               <td>{lista.Arroz.info.preco}</td>
+              <td>{lista.Arroz.info.DataCompra}</td>
               <td>{lista.Arroz.info.unidade}</td>
               <td>{lista.Arroz.info.qtd}</td>
-              <td>{lista.Arroz.info.DataCompra}</td>
               <td><input className={styles.container} type="checkbox"  name="bought"/></td>
             </tr>
           </tbody>
