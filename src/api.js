c91cf65f6185f79959a5a879bef3481d7382b760
Tadeu Junior <tadeu_junior@outlook.com>
2023-03-27 15:09:27 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const apiService = () => {
   });
   axiosInstance.interceptors.request.use(
     (config) => { //apenas para testar a Lista
-      const store = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkY2ZhZjZjMS0xMDBjLTRiZTgtYjE5My03OGE0YzBmOTRlNTgiLCJlbWFpbCI6InRhZGV1X2p1bmlvckBvdXRsb29rLmNvbSIsImlhdCI6MTY3NzI0MzIxNSwiZXhwIjoxNjc3NDE2MDE1fQ._6tEObQ7yc9kc_43XaS6t3ouSqBCs34h2miGNIUPoco";
+      const store = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkY2ZhZjZjMS0xMDBjLTRiZTgtYjE5My03OGE0YzBmOTRlNTgiLCJlbWFpbCI6InRhZGV1X2p1bmlvckBvdXRsb29rLmNvbSIsImlhdCI6MTY3OTk0MDIxNSwiZXhwIjoxNjgwMTEzMDE1fQ.oRj6ll_nY8L3-Ni0QCFycj5LUOcmKSQrX14YJl75Vjs";
       if (store) {
         
         config.headers["Authorization"] = ` Bearer ${store}`;
